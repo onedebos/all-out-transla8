b2428eefdd1b5f162be9d44153da1390eaf13403
Adebola Adeniran <adebola@Adebolas-MacBook-Pro.local>
2021-07-16 18:22:38 +0100

    add HeadMeta tags

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import 'tailwindcss/tailwind.css'
 import { Provider } from 'react-redux';
 import store from '../src/utils/slices/store';
+import HeadMeta from '../src/components/HeadMeta';
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<Provider store={store}>
+			<HeadMeta />
 			<Component {...pageProps} />
 		</Provider>
 	);
---
+++ src/components/HeadMeta/index.js
@@ -0,0 +1,10 @@
+import Head from 'next/head';
+
+export default function HeadMeta() {
+	return (
+		<Head>
+			<title>All-out-Transla8</title>
+			<meta property="og:title" content="All-out-Transla8" key="title" />
+		</Head>
+	);
+}
